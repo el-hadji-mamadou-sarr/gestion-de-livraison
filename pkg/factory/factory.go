ba7eb8798bcb1921317a667d7c0f4e50139534af
Arthur277 <arthurdeumenitsako@gmail.com>
2025-03-12 13:03:22 +0100

    add comment

--- pkg/factory/factory.go
+++ pkg/factory/factory.go
@@ -6,8 +6,9 @@ import (
 	"github.com/el-hadji-mamadou-sarr/gestion-de-livraison.git/pkg/interfaces"
 	"github.com/el-hadji-mamadou-sarr/gestion-de-livraison.git/pkg/models"
 )
-
+// permet de créer dynamiquement des instances de méthodes de transport
 func GetTransportMethod(method string) (interfaces.TransportMethod, error) {
+	//pour les différents cas
 	switch method {
 	case "truck":
 		return &models.Truck{ID: "T123", Capacity: 10}, nil
